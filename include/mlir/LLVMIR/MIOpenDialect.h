cf6939fc183b70eb9cf6f97ac996b2ef109d1b76
Wen-Heng (Jack) Chung <whchung@gmail.com>
2019-09-23 14:53:02 -0500

    Initial commit of MIOpen dialect.
    Has nothing useful yet. Simply builds.

---
+++ include/mlir/LLVMIR/MIOpenDialect.h
@@ -0,0 +1,44 @@
+//===- MIOpenDialect.h - MLIR MIOpen IR dialect ---------------------*- C++ -*-===//
+//
+// Copyright 2019 The MLIR Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+//
+// This file defines the MIOpen IR dialect in MLIR, containing MIOpen operations and
+// MIOpen specific extensions to the LLVM type system.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_LLVMIR_MIOPENDIALECT_H_
+#define MLIR_LLVMIR_MIOPENDIALECT_H_
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+namespace mlir {
+namespace miopen {
+
+/////// Ops /////
+//#define GET_OP_CLASSES
+//#include "mlir/LLVMIR/MIOpenOps.h.inc"
+
+class MIOpenDialect : public Dialect {
+public:
+  explicit MIOpenDialect(MLIRContext *context);
+};
+
+} // namespace miopen
+} // namespace mlir
+
+#endif /* MLIR_LLVMIR_MIOpenDIALECT_H_ */
+
---
+++ lib/LLVMIR/IR/MIOpenDialect.cpp
@@ -0,0 +1,62 @@
+//===- MIOpenDialect.cpp - MIOpen IR Ops and Dialect registration -------------===//
+//
+// Copyright 2019 The MLIR Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+//
+// This file defines the types and operation details for the MIOpen IR dialect in
+// MLIR, and the LLVM IR dialect.  It also registers the dialect.
+//
+// The MIOpen dialect only contains GPU specific additions on top of the general
+// LLVM dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/LLVMIR/MIOpenDialect.h"
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/StandardTypes.h"
+#include "mlir/LLVMIR/LLVMDialect.h"
+#include "llvm/AsmParser/Parser.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Support/SourceMgr.h"
+
+namespace mlir {
+namespace miopen {
+
+//===----------------------------------------------------------------------===//
+// MIOpenDialect initialization, type parsing, and registration.
+//===----------------------------------------------------------------------===//
+
+MIOpenDialect::MIOpenDialect(MLIRContext *context) : Dialect("miopen", context) {
+//  addOperations<
+//#define GET_OP_LIST
+//#include "mlir/LLVMIR/MIOpenOps.cpp.inc"
+//      >();
+
+  // Support unknown operations because not all MIOpen operations are registered.
+  allowUnknownOperations();
+}
+
+//#define GET_OP_CLASSES
+//#include "mlir/LLVMIR/MIOpenOps.cpp.inc"
+
+static DialectRegistration<MIOpenDialect> miopenDialect;
+
+} // namespace miopen
+} // namespace mlir
